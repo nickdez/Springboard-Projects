328242fc496607bb63c458ea894752a45ed77975
nickdez <105134883+nickdez@users.noreply.github.com>
2022-12-02 19:23:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,195 @@
+"use strict";
+
+// So we don't have to keep re-finding things on page, find DOM elements once:
+
+const $body = $("body");
+
+const $storiesLoadingMsg = $("#stories-loading-msg");
+const $allStoriesList = $("#all-stories-list");
+const $favoritedStories = $("#favorited-stories");
+const $ownStories = $("#my-stories");
+
+// selector that finds all three story lists
+const $storiesLists = $(".stories-list");
+
+const $loginForm = $("#login-form");
+const $signupForm = $("#signup-form");
+
+const $submitForm = $("#submit-form");
+
+const $navSubmitStory = $("#nav-submit-story");
+const $navLogin = $("#nav-login");
+const $navUserProfile = $("#nav-user-profile");
+const $navLogOut = $("#nav-logout");
+
+const $userProfile = $("#user-profile");
+
+/** To make it easier for individual components to show just themselves, this
+ * is a useful function that hides pretty much everything on the page. After
+ * calling this, individual components can re-show just what they want.
+ */
+
+function hidePageComponents() {
+  const components = [
+    $storiesLists,
+    $submitForm,
+    $loginForm,
+    $signupForm,
+    $userProfile
+  ];
+  components.forEach(c => c.hide());
+}
+
+/** Overall function to kick off the app. */
+
+async function start() {
+  console.debug("start");
+
+  // "Remember logged-in user" and log in, if credentials in localStorage
+  await checkForRememberedUser();
+  await getAndShowStoriesOnStart();
+
+  // if we got a logged-in user
+  if (currentUser) updateUIOnUserLogin();
+}
+
+// Once the DOM is entirely loaded, begin the app
+
+console.warn("HEY STUDENT: This program sends many debug messages to" +
+  " the console. If you don't see the message 'start' below this, you're not" +
+  " seeing those helpful debug messages. In your browser console, click on" +
+  " menu 'Default Levels' and add Verbose");
+$(start);
+js / user.js
+"use strict";
+
+// global to hold the User instance of the currently-logged-in user
+let currentUser;
+
+/******************************************************************************
+ * User login/signup/login
+ */
+
+/** Handle login form submission. If login ok, sets up the user instance */
+
+async function login(evt) {
+  console.debug("login", evt);
+  evt.preventDefault();
+
+  // grab the username and password
+  const username = $("#login-username").val();
+  const password = $("#login-password").val();
+
+  // User.login retrieves user info from API and returns User instance
+  // which we'll make the globally-available, logged-in user.
+  currentUser = await User.login(username, password);
+
+  $loginForm.trigger("reset");
+
+  saveUserCredentialsInLocalStorage();
+  updateUIOnUserLogin();
+}
+
+$loginForm.on("submit", login);
+
+/** Handle signup form submission. */
+
+async function signup(evt) {
+  console.debug("signup", evt);
+  evt.preventDefault();
+
+  const name = $("#signup-name").val();
+  const username = $("#signup-username").val();
+  const password = $("#signup-password").val();
+
+  // User.signup retrieves user info from API and returns User instance
+  // which we'll make the globally-available, logged-in user.
+  currentUser = await User.signup(username, password, name);
+
+  saveUserCredentialsInLocalStorage();
+  updateUIOnUserLogin();
+
+  $signupForm.trigger("reset");
+}
+
+$signupForm.on("submit", signup);
+
+/** Handle click of logout button
+ *
+ * Remove their credentials from localStorage and refresh page
+ */
+
+function logout(evt) {
+  console.debug("logout", evt);
+  localStorage.clear();
+  location.reload();
+}
+
+$navLogOut.on("click", logout);
+
+/******************************************************************************
+ * Storing/recalling previously-logged-in-user with localStorage
+ */
+
+/** If there are user credentials in local storage, use those to log in
+ * that user. This is meant to be called on page load, just once.
+ */
+
+async function checkForRememberedUser() {
+  console.debug("checkForRememberedUser");
+  const token = localStorage.getItem("token");
+  const username = localStorage.getItem("username");
+  if (!token || !username) return false;
+
+  // try to log in with these credentials (will be null if login failed)
+  currentUser = await User.loginViaStoredCredentials(token, username);
+}
+
+/** Sync current user information to localStorage.
+ *
+ * We store the username/token in localStorage so when the page is refreshed
+ * (or the user revisits the site later), they will still be logged in.
+ */
+
+function saveUserCredentialsInLocalStorage() {
+  console.debug("saveUserCredentialsInLocalStorage");
+  if (currentUser) {
+    localStorage.setItem("token", currentUser.loginToken);
+    localStorage.setItem("username", currentUser.username);
+  }
+}
+
+/******************************************************************************
+ * General UI stuff about users & profiles
+ */
+
+/** When a user signs up or registers, we want to set up the UI for them:
+ *
+ * - show the stories list
+ * - update nav bar options for logged-in user
+ * - generate the user profile part of the page
+ */
+
+async function updateUIOnUserLogin() {
+  console.debug("updateUIOnUserLogin");
+
+  hidePageComponents();
+
+  // re-display stories (so that "favorite" stars can appear)
+  putStoriesOnPage();
+  $allStoriesList.show();
+
+  updateNavOnLogin();
+  generateUserProfile();
+}
+
+/** Show a "user profile" part of page built from the current user's info. */
+
+function generateUserProfile() {
+  console.debug("generateUserProfile");
+
+  $("#profile-name").text(currentUser.name);
+  $("#profile-username").text(currentUser.username);
+  $("#profile-account-date").text(currentUser.createdAt.slice(0, 10));
+}
+
